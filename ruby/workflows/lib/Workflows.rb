9807af256b7d8f0560fea9a13bc32836cb146407
tony <amirsgarbunk@gmail.com>
2013-08-10 18:12:51 -0700

    tidied up tied status module into workflow gem

--- ruby/workflows/lib/Workflows.rb
+++ ruby/workflows/lib/Workflows.rb
@@ -1,5 +1,7 @@
 require 'mongooz'
-require 'workflows/workflow_base.rb'
-require 'workflows/workflow_objects.rb'
-
 Mongooz.defaults(:db=>'workflows')
+
+require 'workflows/workflow_base.rb'
+require 'workflows/status/workflow_status.rb'
+
+require 'workflows/workflow_objects.rb'
